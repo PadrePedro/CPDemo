1df7f66159cfb5ebd9e48191d598b831165dc81d
PadrePedro <pliaw@zeal.com>
2015-02-01 13:27:30 -0800

    new files

---
+++ app/src/main/java/com/example/pedro/cpdemo/ResponseDialogFragment.java
@@ -0,0 +1,45 @@
+package com.example.pedro.cpdemo;
+
+import com.example.pedro.cpdemo.R;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by pedro on 2/1/15.
+ */
+public class ResponseDialogFragment extends DialogFragment {
+
+    private String title;
+    private String message;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view = getActivity().getLayoutInflater().inflate(R.layout.response_view, null);
+        builder.setView(view);
+        EditText textView = (EditText)view.findViewById(R.id.responseTextView);
+        textView.setText(message);
+        builder.setTitle(title).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        return builder.create();
+    }
+
+    public static void showResponse(FragmentActivity activity, int id, String response) {
+        ResponseDialogFragment dialog = new ResponseDialogFragment();
+        dialog.title = String.format("Fetch %d", id);
+        dialog.message = response;
+        dialog.show(activity.getFragmentManager(), dialog.getClass().getSimpleName());
+    }
+}
